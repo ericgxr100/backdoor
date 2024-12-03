43941e2e1858c23e33a055883569b6d429da79e4
Eric Garcia <105328078+ericgxr100@users.noreply.github.com>
2024-12-03 12:16:47 +0100

    Update listener.py

--- listener.py
+++ listener.py
@@ -7,19 +7,19 @@ s = socket(AF_INET, SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 
-print('Escuchando en 0.0.0.0:%s' %str(PORT))
+print('Listening in 0.0.0.0:%s' %str(PORT))
 
 s.listen(1)
 
 conn, addr = s.accept()
 
-print("[*]Nueva conexión entrante desde => %s:%s" % (str(addr[0]), str(addr[1])))
+print("[*]New incoming connection from => %s:%s" % (str(addr[0]), str(addr[1])))
 
 while 1:
     command = raw_input(str(addr[0] + "$: "))
     conn.send(command)
     if command == "quit":
-        print("Cerrando puerto en escucha")
+        print("Closing listening port")
         break
     data = conn.recv(4096)
     print(data)
